cb9f63225ecb25fc1cceee02ba0abaef9926f5e7
vinsxzero <vini404310@gmail.com>
2026-01-19 11:35:19 -0300

    pagina de contato finalizada

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,92 @@
+//tratando formulário
+class FormSubmit {
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.button = document.querySelector(settings.button);
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+  getFormObject() {
+    const formObject = {};
+    const fields = document.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
+  validateForm() {
+    const fields = this.form.querySelectorAll("[name]");
+    let valid = true;
+    fields.forEach((field) => {
+      field.classList.remove("error-field");
+      if (!field.value.trim()) {
+        valid = false;
+        field.classList.add("error-field");
+        return;
+      }
+      if (field.type === "email") {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(field.value)) {
+          valid = false;
+          field.classList.add("error-field");
+        }
+      }
+    });
+    return valid;
+  }
+  onSubmission(event) {
+    event.preventDefault();
+    event.target.disabled = true;
+    event.target.innerText = "Enviando...";
+  }
+  async sendForm(event) {
+    if (!this.validateForm()) {
+      alert("Por favor, preencha todos os campos  corretamente.");
+      return;
+    }
+    try {
+      this.onSubmission(event);
+      await fetch(this.url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(this.getFormObject()),
+      });
+      this.displaySuccess();
+    } catch (error) {
+      this.displayError();
+      throw new Error(error);
+    }
+  }
+
+  init() {
+    if (this.form) {
+      this.button.addEventListener("click", this.sendForm);
+    }
+    return this;
+  }
+}
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<h1  class='success'>Mensagem enviada com sucesso!</h1>",
+  error: "<h1 class='error>Não foi possível enviar sua mensagem.</h1>",
+});
+formSubmit.init();
+//definindo ano
 var year = new Date().getFullYear();
 
 var rights = document.getElementById("year");
