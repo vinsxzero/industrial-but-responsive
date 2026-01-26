d9f592ba2affa0ba05bbb4eab4de7489969e7465
vinsxzero <vini404310@gmail.com>
2026-01-26 16:42:45 -0300

    fixed style and objectives section

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -93,11 +93,24 @@ var rights = document.getElementById("year");
 rights.textContent = year;
 //selecionando item de mission
 const missionRow = document.querySelectorAll(".object .row");
-
+const contents = document.querySelectorAll(".content");
+let current = document.querySelector(".content.show");
 missionRow.forEach((row) => {
   const arrow = row.querySelector(".arrow");
 
   arrow.addEventListener("click", () => {
-    row.classList.toggle("selected");
+    const targetId = row.dataset.target;
+
+    missionRow.forEach((i) => i.classList.remove("selected"));
+    row.classList.add("selected");
+
+    contents.forEach((content) => {
+      content.classList.add("inactive");
+      content.classList.remove("selected");
+
+      if (content.id === targetId) {
+        content.classList.add("selected");
+      }
+    });
   });
 });
